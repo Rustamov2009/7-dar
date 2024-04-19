d1bcbcdf8c17671c08dadbc6ee5747e0de602325
Rustamov2009 <firdavsr000@gmail.com>
2024-04-19 17:34:38 +0500

    homevork

---
+++ script.js
@@ -0,0 +1,9 @@
+let mevalar = [olma,nok,uzum,anor ];
+alert (mevalar +  `bu arrayning uzunligi` + mevalar.length);
+let sorov = confirm (`malumotni ochirasizmi`);
+console.log(sorov);
+if (sorov==true) {
+    mevalar.pop()
+  
+}else (sorov==false)
+    alert (mevalar)
